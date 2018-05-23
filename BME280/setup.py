42b5932503e1ac51a230fe99a15aa9bad155562b
Riccardo Zoncada <riccardo.zoncada@zoncrd>
2018-05-23 22:02:27 +0200

    added setup scripts

---
+++ BME280/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='BME280',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='',
+    author_email='',
+    description='BME280 sensor class'
+)
---
+++ SDS011/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='SDS011',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='zoncrd',
+    author_email='zoncrd@nutrialug.pv',
+    description='SDS011 Nova sensor class'
+)
